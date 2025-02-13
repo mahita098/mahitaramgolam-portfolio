980af6ade8e624f1cdbe5fd4dd28e85b43823c50
Mahita Ramgolam <mahita098@gmail.com>
2025-02-13 22:54:55 +0400

    updating

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -7,10 +7,11 @@ const ProjectCards = ({
   projdescription,
   projimage,
   projtags,
+  projgit,
 }) => {
   return (
     <div className="cards pb-4 mt-3 mb-5 px-5 md:px-0">
-      <a href={projlink}>
+      <a href={projlink} target="_blank">
         <Card.Root
           flexDirection={{ base: "column", md: "row" }}
           overflow="hidden"
@@ -25,30 +26,41 @@ const ProjectCards = ({
             alt="Caffe Latte"
           />
           <Box>
-            <Card.Body className="p-0 flex flex-col justify-between py-2 px-4 gap-2">
-              <Card.Title mb="2">{projtitle}</Card.Title>
+            <Card.Body className="p-0 flex flex-col justify-between h-full ">
+              <div className="flex flex-col py-2 px-4 gap-2 w-full md:w-[335px] h-full ">
+                <Card.Title className="text-xl font-bold" mb="2">
+                  {projtitle}
+                </Card.Title>
 
-              <Card.Description>{projdescription}</Card.Description>
-              <HStack className="flex flex-wrap" mt="4">
-                {projtags.map((tag, index) => {
-                  return <Badge key={index}>{tag}</Badge>;
-                })}
-              </HStack>
+                <Card.Description>{projdescription}</Card.Description>
+                <HStack className="flex flex-wrap" mt="4">
+                  {projtags.map((tag, index) => {
+                    return (
+                      <Badge className="text-sm" key={index}>
+                        {tag}
+                      </Badge>
+                    );
+                  })}
+                </HStack>
+              </div>
 
-              <div className="flex flex-row gap-2 justify-between align-middle">
-                <svg
-                  className="md:w-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    fill-rule="evenodd"
-                    d="M11.999 1C5.926 1 1 5.925 1 12c0 4.86 3.152 8.983 7.523 10.437c.55.102.75-.238.75-.53c0-.26-.009-.952-.014-1.87c-3.06.664-3.706-1.475-3.706-1.475c-.5-1.27-1.221-1.61-1.221-1.61c-.999-.681.075-.668.075-.668c1.105.078 1.685 1.134 1.685 1.134c.981 1.68 2.575 1.195 3.202.914c.1-.71.384-1.195.698-1.47c-2.442-.278-5.01-1.222-5.01-5.437c0-1.2.428-2.183 1.132-2.952c-.114-.278-.491-1.397.108-2.91c0 0 .923-.297 3.025 1.127A10.5 10.5 0 0 1 12 6.32a10.5 10.5 0 0 1 2.754.37c2.1-1.424 3.022-1.128 3.022-1.128c.6 1.514.223 2.633.11 2.911c.705.769 1.13 1.751 1.13 2.952c0 4.226-2.572 5.156-5.022 5.428c.395.34.747 1.01.747 2.037c0 1.47-.014 2.657-.014 3.017c0 .295.199.637.756.53C19.851 20.979 23 16.859 23 12c0-6.075-4.926-11-11.001-11"
-                  />
-                </svg>
-                <Button src={projlink}>
+              <div className="flex flex-row gap-2 justify-between align-middle px-4 ">
+                <a href={projgit} target="_blank">
+                  <svg
+                    className="md:w-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      d="M11.999 1C5.926 1 1 5.925 1 12c0 4.86 3.152 8.983 7.523 10.437c.55.102.75-.238.75-.53c0-.26-.009-.952-.014-1.87c-3.06.664-3.706-1.475-3.706-1.475c-.5-1.27-1.221-1.61-1.221-1.61c-.999-.681.075-.668.075-.668c1.105.078 1.685 1.134 1.685 1.134c.981 1.68 2.575 1.195 3.202.914c.1-.71.384-1.195.698-1.47c-2.442-.278-5.01-1.222-5.01-5.437c0-1.2.428-2.183 1.132-2.952c-.114-.278-.491-1.397.108-2.91c0 0 .923-.297 3.025 1.127A10.5 10.5 0 0 1 12 6.32a10.5 10.5 0 0 1 2.754.37c2.1-1.424 3.022-1.128 3.022-1.128c.6 1.514.223 2.633.11 2.911c.705.769 1.13 1.751 1.13 2.952c0 4.226-2.572 5.156-5.022 5.428c.395.34.747 1.01.747 2.037c0 1.47-.014 2.657-.014 3.017c0 .295.199.637.756.53C19.851 20.979 23 16.859 23 12c0-6.075-4.926-11-11.001-11"
+                    />
+                  </svg>
+                </a>
+
+                <Button src={projlink} target="_blank">
                   Visit Website{" "}
                   <svg
                     stroke="currentColor"
