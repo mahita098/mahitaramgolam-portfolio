6e7a54d13a1863b950e30af9001fc1949b3695f5
Mahita Ramgolam <mahita098@gmail.com>
2025-02-19 22:15:23 +0400

    error debugging

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist", // Ensure this matches your deployment output directory
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
