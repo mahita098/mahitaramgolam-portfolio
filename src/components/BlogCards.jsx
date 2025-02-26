eaaae0b6e2a48967792a95a932ace4c96d36d8c3
Mahita Ramgolam <mahita098@gmail.com>
2025-02-26 16:29:55 +0400

    updates

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 
-const BlogCards = ({ pblogimage, pblogtitle, pblogdescription, pbloglink }) => {
+const BlogCards = ({
+  pblogimage,
+  pblogtitle,
+  pblogdescription,
+  pbloglink,
+  pblogdate,
+}) => {
   return (
     <div className="cards pb-4 mt-3 mb-5 px-0">
       <a href={pbloglink}>
-        <div className="card bg-base-100 image-full w-auto md:w-96 shadow-xl">
+        <div className="card image-full w-auto md:w-96 ">
           <figure>
             <img src={pblogimage} alt="sydney" />
           </figure>
           <div className="card-body p-8">
-            <h2 className="card-title mx-auto">{pblogtitle}</h2>
-            <p className="text-center">{pblogdescription}</p>
+            <h2 className="card-title mx-auto text-slate-200">{pblogtitle}</h2>
+            <p className="text-center text-slate-200 drop-shadow-lg">
+              {pblogdescription}
+            </p>
             <div className="card-actions justify-end">
-              <p className="text-primary flex justify-end">16 Feb 2025</p>
+              <p className="text-primary flex justify-end">{pblogdate}</p>
             </div>
           </div>
         </div>
